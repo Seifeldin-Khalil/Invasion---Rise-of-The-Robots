ac2ca9ab6dfd04b5024c9cd461dc7d9f422b0e86
Seifeldin-Khalil <96579102+Seifeldin-Khalil@users.noreply.github.com>
2021-12-25 11:25:58 +0200

    Player shooting
    Player bullet destroys after 3s seconds if it does not collide with anything

--- Survior - Rise of The Robots/Assets/Scripts/EnemyShooting.cs
+++ Survior - Rise of The Robots/Assets/Scripts/EnemyShooting.cs
@@ -28,7 +28,7 @@ public class EnemyShooting : MonoBehaviour
     {
         Destroy(gameObject);
     }
-    private void OnCollisionEnter2D(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
