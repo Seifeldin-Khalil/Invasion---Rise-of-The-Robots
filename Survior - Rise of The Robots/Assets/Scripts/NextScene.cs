2181822215d5e7b174e2cd1b3392e665e10ea2a5
Maiadaa <maiadahkhaled@gmail.com>
2021-12-27 14:40:00 +0200

    gaeg

---
+++ Survior - Rise of The Robots/Assets/Scripts/NextScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadover()
+    {
+        SceneManager.LoadScene("Level2Scene2");
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        loadover();
+    } 
+}
