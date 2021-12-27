6ac1a2516ef64ec73acb5c56a1ff4b56be04a15f
Maiadaa <maiadahkhaled@gmail.com>
2021-12-27 19:50:29 +0200

    introScene
    design

---
+++ Survior - Rise of The Robots/Assets/Scripts/NavigationController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationController : MonoBehaviour
+{
+    // Main Menu Scene
+    public void GoToIntroScene()
+    {
+        Application.LoadLevel(0);
+    }
+
+    // Level 1 Scene 1 
+    public void GoToGameScene11()
+    {
+        Application.LoadLevel(1);
+    }
+    // Level 1 Scene 2 
+    public void GoToGameScene12()
+    {
+        Application.LoadLevel(2);
+    }
+
+    // Level 2 Scene 1 
+    public void GoToGameScene21()
+    {
+        Application.LoadLevel(3);
+    }
+    // Level 2 Scene 2 
+    public void GoToGameScene22()
+    {
+        Application.LoadLevel(4);
+    }
+
+    // Level 3 Scene 1 
+    public void GoToGameScene31()
+    {
+        Application.LoadLevel(5);
+    }
+    // Level 3 Scene 2 
+    public void GoToGameScene32()
+    {
+        Application.LoadLevel(5);
+    }
+
+    // Level 4 Scene 1 
+    public void GoToGameScene41()
+    {
+        Application.LoadLevel(6);
+    }
+    // Level 4 Scene 2 
+    public void GoToGameScene42()
+    {
+        Application.LoadLevel(7);
+    }
+
+    public void GoToGameOverScene()
+    {
+        Application.LoadLevel(8);
+    }
+    public void GoToVictoryScene()
+    {
+        Application.LoadLevel(9);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
