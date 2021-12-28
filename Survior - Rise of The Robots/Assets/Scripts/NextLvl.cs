5eab885cf569f767d878f9d7b261a57208f75c31
Sama Hany <sama193754@bue.edu.eg>
2021-12-28 11:26:01 +0200

    Transition
    Transition between scene1 and scene2 (elli homa mn scene2 to scene1) we l train collider

---
+++ Survior - Rise of The Robots/Assets/Scripts/NextLvl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene1NextLvl3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void loadover()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        loadover();
+    }
+}
