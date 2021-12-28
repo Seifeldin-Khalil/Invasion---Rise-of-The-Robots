3dd3368023b4150cb5d20a9b1fb142d309e38832
Maiadaa <maiadahkhaled@gmail.com>
2021-12-28 12:36:04 +0200

    fv

--- Survior - Rise of The Robots/Assets/Scripts/Level2NextScene.cs
+++ Survior - Rise of The Robots/Assets/Scripts/Level2NextScene.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level2NextScene : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     void loadover()
     {
         SceneManager.LoadScene("Level2Scene2");
