47ee281bcdc62e918915a182f71c14ab6db46031
Sama Hany <sama193754@bue.edu.eg>
2021-12-28 11:37:04 +0200

    2nd trail

--- Survior - Rise of The Robots/Assets/Scripts/NextLvl.cs
+++ Survior - Rise of The Robots/Assets/Scripts/NextLvl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Scene1NextLvl3 : MonoBehaviour
+public class NextLvl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
