51f2622b08dd8e4ca874b8e25b58cadfeb94a893
Maiadaa <maiadahkhaled@gmail.com>
2021-12-29 12:58:24 +0200

    edits

---
+++ Survior - Rise of The Robots/Assets/Scripts/VictoryTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadover()
+    {
+        SceneManager.LoadScene("VictoryScene");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            loadover();
+        }
+
+    }
+}
